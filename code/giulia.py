c2c3988067e77040e450f1241fc9bfcfcf9a3553
GiuliaDAngelo <giulia.dangelo.1990@gmail.com>
2023-09-02 12:31:35 +0200

    last improvements

--- code/giulia.py
+++ code/giulia.py
@@ -12,7 +12,9 @@ from bimvee.importIitYarp import importIitYarp
 from bimvee.importProph import importProph
 from torchvision.transforms import InterpolationMode
 from skimage.transform import rescale, resize, downscale_local_mean
+from torchvision.transforms import Resize
 import cv2
+import torch.nn.functional as F
 
 
 
@@ -30,10 +32,13 @@ def run(angle_shift, fltr_resize_perc, time_wnd_frames, rec, polarity, batch_fra
     fig.suptitle(f'VM filters size ({filter.shape[0]},{filter.shape[0]})', fontsize=16)
 
     #show filters
-    show_fltFLAG = True
+    show_fltFLAG = False
+    show_input_image = False
+    shhow_partial_sal_map = True
+
     if show_fltFLAG:
-        for i in range(8):
-            if i < 4:
+        for i in range(len(angles)):
+            if i < (len(angles)/2):
                 axes[0, i].set_title(f"{angles[i]} grad")
                 axes[0, i].imshow(filters[i])
             else:
@@ -66,39 +71,68 @@ def run(angle_shift, fltr_resize_perc, time_wnd_frames, rec, polarity, batch_fra
         torchvision.transforms.CenterCrop((sensor_size[1]-1, sensor_size[0]-1)),
     ])
 
-    frames = transforms(rec)
+    frames = transforms(rec).float()
     torch.empty((2, 3), dtype=torch.int64)
-    #HERE   i frame ora sembrano avere la stessa dimensione pero' c'e' un crop strano dell'immagine, controlla
     num_pyr = 5
-    for pyr in range(1, num_pyr+1):
-        res = (int((frames[10, 0].shape[0])/pyr), int((frames[10, 0].shape[1])/pyr))
+    resolutions = [[0] * 2] * num_pyr
+    for pyr in range(0, num_pyr):
+        resolutions[pyr] = [int((frames[10, 0].shape[0]) / (pyr+1)), int((frames[10, 0].shape[1]) / (pyr+1))]
+    # num_pyr is the number of the scales I want my pyramid
+    sal_map= torch.empty((batch_frames[1]-batch_frames[0]), 1, resolutions[0][0], resolutions[0][1], dtype=torch.float32)
+    for scale in range(0, num_pyr):
+        res = resolutions[scale]
+        #creation empty tensor to collect frames of the same resolution, tensor or 337 matrixes (480,640)
+        #frames[336, 0]), tensor of 337 frames
+        #frames_ch, tensor containng the frames with the new resolution (batch_frame size, 1, camera width, camera hight)
         count = 0
-        frames_ch = torch.empty(((batch_frames[1]-batch_frames[0]), 1, int((frames[10, 0].shape[0])/pyr), int((frames[10, 0].shape[1])/pyr)), dtype=torch.int64)
+        frames_ch = torch.empty(((batch_frames[1]-batch_frames[0]), 1, res[0], res[1]), dtype=torch.int64)
+        #running the model for multiple resolutions
         for frame in range(batch_frames[0], batch_frames[1]):
-            frames_ch[count] = frames[frame].resize_(1, int((frames[10, 0].shape[0])/pyr), int((frames[10, 0].shape[1])/pyr))
+            frames_ch[count] = rescale_tensor(frames[frame].unsqueeze(0), res[0], res[1])
             count+=1
         # this leaves us with some 337 time steps.
         print(res)
         frames_ch.shape
-        plt.figure()
-        plt.imshow(frames_ch[9, 0])
+        if show_input_image:
+            plt.figure()
+            plt.imshow(frames_ch[9, 0])
         # now we feed the data to our network! Because my computer has little memory, I only feed 10 specific time steps
         with torch.no_grad():
             output = net(frames_ch.float())
         output.shape
+        sal_map_ori = torch.empty(output.shape[0], 1, output.shape[2],output.shape[3], dtype=torch.float32)
         # in the end we can plot the sal map
-        fig, axes = plt.subplots(2, 4, figsize=(10, 5))
-        for i in range(8):
-            if i < 4:
-                axes[0, i].set_title(f"{angles[i]} grad")
-                axes[0, i].imshow(output[0, i])
-            else:
-                axes[1, i-4].set_title(f"{angles[i]} grad")
-                axes[1, i-4].imshow(output[0, i])
-        plt.show()
-        print('end')
+        if shhow_partial_sal_map:
+            fig, axes = plt.subplots(2, 4, figsize=(10, 5))
+            for i in range(len(angles)):
+                if i < (int(len(angles)/2)):
+                    axes[0, i].set_title(f"{angles[i]} grad")
+                    axes[0, i].imshow(output[0, i])
+                else:
+                    axes[1, i-4].set_title(f"{angles[i]} grad")
+                    axes[1, i-4].imshow(output[0, i])
+                sal_map_ori += output[0, i]
+            plt.show()
+            #understand the contribute of each orientation and scale, possible problem with scales
+            plt.figure()
+            plt.imshow(sal_map_ori[2, 0])
+            #here, fare la sal map generale delle scale quindi tornare indietro con lo squeeze
+            plt.title(f'scale: {scale}, partial sal map orientations', fontsize=16)
+            plt.show()
+            unsq_sal_map_ori = rescale_tensor(sal_map_ori, resolutions[0][0], resolutions[0][1])
+            sal_map += unsq_sal_map_ori
+            plt.figure()
+            plt.imshow(sal_map[2, 0])
+            plt.title(f'final sal map', fontsize=16)
+            plt.show()
+    print('end run')
 
-
+def rescale_tensor(inp, xout_size, yout_size):
+    x = torch.linspace(-1, 1, yout_size).repeat(xout_size, 1)
+    y = torch.linspace(-1, 1, xout_size).view(-1, 1).repeat(1, yout_size)
+    grid = torch.cat((x.unsqueeze(2), y.unsqueeze(2)), 2)
+    grid = grid.unsqueeze_(0).repeat(inp.shape[0], 1, 1, 1)
+    return F.grid_sample(inp, grid)
 
 
 def raw_data(filePathOrName):
@@ -156,7 +190,7 @@ if __name__ == '__main__':
     angle_shift = 45
     time_wnd_frames = 20000 #us
     polarity = 0
-    batch_frames = (100, 110)
+    batch_frames = (100, 110) #frames considered
     ############################################
     ################ data ######################
     ############################################
